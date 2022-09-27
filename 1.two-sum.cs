7db025f322e5eb9cd12c5e41f4cc8cafed5b1f86
Eugene Pok <eugene.pok@meiban.com>
2022-09-27 14:39:27 +0800

    First commit

---
+++ 1.two-sum.cs
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=csharp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        //Create a hash table with key as target searching and value as index matched.
+        Hashtable dic = new Hashtable();
+
+        for(int i = 0; i < nums.Length; i++){
+            if(dic.ContainsKey(nums[i]))
+                return new int[] { (int)dic[nums[i]], i };
+            else
+                dic[target - nums[i]] = i;
+        }
+
+        return new int[] { };
+    }
+}
+// @lc code=end
+
