b976891355f4ff7fe7fed3ddbc1458b0501072bb
Eugene Pok <zcpok97@gmail.com>
2022-10-09 23:49:41 +0800

    uncompleted Q21 and added ListNode class

---
+++ console/ListNode.cs
@@ -0,0 +1,8 @@
+class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val, ListNode next) {
+          this.val = val;
+          this.next = next;
+      }
+  }
