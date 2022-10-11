ab18e427e856e71d22eb1228b902863861ea10c7
Eugene Pok <zcpok97@gmail.com>
2022-10-11 11:24:36 +0800

    Completed Q226

--- console/Program.cs
+++ console/Program.cs
@@ -1,25 +1,18 @@
 ﻿using System.Collections;
 
-bool IsPalindrome(string s) {
-    int l = 0;
-    int r = s.Length - 1;
-    char left = Char.ToLower(s[l]);
-    char right = Char.ToLower(s[r]);
-    while(l < r){
-        if(left >= 97 && left <= 122 && right >= 97 && right <= 122){
-            if (left != right) return false;
-            else {
-                left = Char.ToLower(s[++l]);
-                right = Char.ToLower(s[--r]);
-                continue;
-            }
-        }
-        if(left < 97 || left > 122)
-            left = Char.ToLower(s[++l]);
-        if((right < 97 || right > 122))
-            right = Char.ToLower(s[--r]);
-    }
-    return true;
+TreeNode InvertTree(TreeNode root) {
+    if(root == null)
+        return root;
+    if(root.left != null)
+        InvertTree(root.left);
+    if(root.right != null)
+        InvertTree(root.right);
+    var temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    return root;
 }
 
-Console.WriteLine(IsPalindrome("0P"));
+var root = InvertTree(new TreeNode(4, new TreeNode(2, null, new TreeNode(3)),
+new TreeNode(7)));
+Console.WriteLine();
