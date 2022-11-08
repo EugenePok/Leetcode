e8e624af43a9afbeb1c610a62ef61147f754d67c
Eugene Pok <zcpok97@gmail.com>
2022-11-08 17:20:30 +0800

    Completed Problems 70

--- 70.climbing-stairs.cs
+++ 70.climbing-stairs.cs
@@ -7,7 +7,11 @@
 // @lc code=start
 public class Solution {
     public int ClimbStairs(int n) {
-        
+        int a = 1, b = 1;
+        while(n-- > 0){
+            a = (b += a) - a;
+        }
+        return a;
     }
 }
 // @lc code=end
