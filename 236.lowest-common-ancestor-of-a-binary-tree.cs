32e2cecff52e814d139bb67937ee826ea1b107fa
Eugene Pok <zcpok97@gmail.com>
2023-03-02 17:26:38 +0800

    Completed Problems 236, 981

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=236 lang=csharp
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null || p == root || q == root) return root;
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        return right == null ? left : root;
+    }
+}
+// @lc code=end
+
